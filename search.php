44344763a053b61f6be469e057769c6580ffb08a
John Douceur <johndo@microsoft.com>
2012-12-10 16:46:11 -0800

    calling framework for randwalk rank method

--- search.php
+++ search.php
@@ -322,6 +322,8 @@ function tagaction() {
 		$r->irv();
 	    else if ($method == "range")
 		$r->rangevote();
+	    else if ($method == "randwalk")
+		$r->randwalk();
 	    else if ($method == "civs")
 		$r->civsrp();
 	    else
